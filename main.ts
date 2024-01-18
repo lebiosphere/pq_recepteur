438cac7f69f640b6c01824bb81bd4cfeb0f407cf
lebiosphere <lebiosphere@gmail.com>
2024-01-18 09:37:33 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    music.ringTone(262)
-    if (receivedNumber == 1) {
-        led.plot(receivedNumber % 5, Math.floor(receivedNumber / 5))
-    }
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+    led.plot(receivedNumber % 5, Math.floor(receivedNumber / 5))
+    basic.pause(100)
+    basic.clearScreen()
 })
 radio.setGroup(43)
+basic.showIcon(IconNames.Heart)
